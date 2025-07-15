7775c8495d5ec6687d7cbb3f125cca8246c3ad0f
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-07-15 12:52:40 +0100

    Create nix-setup.sh

---
+++ nix-setup.sh
@@ -0,0 +1,36 @@
+title="""
+	[+][+][+][+][+][+][+][+][+][+][+][+][+][+][+][+][+]
+	[+]                                             [+]
+	[+] A³ - Agile Asset Analyzer - Setup Script    [+]
+	[+]										                          [+]
+	[+] THIS SCRIPT WILL SET UP THE NECESSARY       [+]
+	[+] TOOLS AND SCRIPTS THAT ARE REQUIRED FOR     [+]
+	[+] A³ TO RUN PROPERLY. THIS COULD TAKE         [+]
+	[+] ANYWHERE FROM A FEW SECONDS TO SEVERAL      [+]
+	[+] MINUTES, DEPENDING ON WHICH PACKAGES YOU    [+]
+	[+] ALREADY HAVE INSTALLED, AND HOW FAST YOUR   [+]
+	[+] INTERNET IS. PLEASE BE PATIENT.             [+]
+	[+] ONCE ALL OF THE NECESSARY FEATURES HAVE     [+]
+	[+] BEEN INSTALLED, YOU WILL BE NOTIFIED.       [+]
+	[+]                                             [+]
+	[+] MEANWHILE, YOU CAN RUN './aaa -help' TO     [+]
+	[+] READ UP ON HOW THIS TOOL WORKS AND HOW TO   [+]
+	[+] EFFICIENTLY USE IT.                         [+]
+	[+]                                             [+]
+	[+]      - MLT (Bug0xF4 Inc.                    [+]
+	[+]                                             [+]
+	[+][+][+][+][+][+][+][+][+][+][+][+][+][+][+][+][+]
+"""
+# terminal colors 
+BLU="\e[94m"
+RED="\e[91m"
+RST="\e[0m"
+GRN="\e[92m"
+YEL="\e[93m"
+
+# output title 
+echo -e $GRN
+cat <<EOF
+$title
+EOF
+echo -e $RST
